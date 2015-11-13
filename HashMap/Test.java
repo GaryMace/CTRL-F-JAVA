68146f3854868829c2d08a1a16be9eb09910afaa
Gary Mac Elhinney <Gary.mac-elhinney@ucdconnect.ie>
2015-11-13 09:47:56 +0000

    Reformatted test class

--- HashMap/Test.java
+++ HashMap/Test.java
@@ -2,12 +2,14 @@
 public class Test {
 	public static void main(String[] args) {
 		GMap<String, Integer> myMap = new GMap<String, Integer>();
-		
+		System.out.println("Origional HashMap:");
 		myMap.put("a", 1);
 		myMap.put("b", 2);
 		myMap.put("c", 3);
 		//myMap.put("d", 4);
 		myMap.put("e", 5);
+		System.out.print(myMap.toString());
+		System.out.println("\n\nHashMap after duplicate key added:");
 		myMap.put("a", 25);
 		System.out.print(myMap.toString());
 		//System.out.println(myMap.get("a"));
