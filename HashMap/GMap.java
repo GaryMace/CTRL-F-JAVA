9e6c4339d59292e4b988d253cf10a6beecb4641e
Gary Mac Elhinney <Gary.mac-elhinney@ucdconnect.ie>
2015-11-15 19:33:29 +0000

    Added dynamic Rehashing

--- HashMap/GMap.java
+++ HashMap/GMap.java
@@ -2,8 +2,9 @@ import java.util.Arrays;
 import java.util.Iterator;
 
 public class GMap<K, V> {
-	public static final int HASH_MAP_SIZE = 5;
-	GEntry<K, V>[] map;
+	public static int HASH_MAP_SIZE = 5;
+	private GEntry<K, V>[] map;
+
 	public GMap(){
         map =  new GEntry[HASH_MAP_SIZE];
 	}
@@ -18,25 +19,32 @@ public class GMap<K, V> {
 	}
 	
 	public boolean isEmpty() {
-		return size() == 0; 
+        for(GEntry<K, V> entry : map) {
+            if(entry != null)
+                return false;
+        }
+        return true;
 	}
 	
 	public V get(K key) {
-        return map[getHash(key)].getValue();
+        return map[findKey(key)].getValue();
 	}
 	
 	public void put(K key, V value) {
         if(containsKey(key)) {
             remove(key);
         }
-        //TODO: Handle too big
-        map[find(key)] = new GEntry<K, V>(key, value);
+       if(size() == HASH_MAP_SIZE) {
+           reHash();
+       }
+        map[find(key)] = new GEntry<>(key, value);
 	}
 	
 	public V remove(K key) {
-		V value = map[getHash(key)].getValue();
-		map[getHash(key)] = null;
-		
+        int keyIndex = findKey(key);
+        V value = map[keyIndex].getValue();
+
+		map[keyIndex] = null;
 		return value;
 	}
 	
@@ -50,18 +58,17 @@ public class GMap<K, V> {
 
         return new Iterator<GEntry<K, V>>(){
             private int index = 0;
+            private int numEntriesLeft = size();
 
-
-            @Override
             public boolean hasNext() {
-                return index<HASH_MAP_SIZE;
+                return index < HASH_MAP_SIZE && numEntriesLeft > 0;
             }
 
-            @Override
             public GEntry<K, V> next() {
                 while(map[index] == null) {
                     index++;
                 }
+                numEntriesLeft--;
                 return map[index++];
             }
         };
@@ -71,18 +78,17 @@ public class GMap<K, V> {
 
         return new Iterator<K>(){
             private int index = 0;
+            private int numEntriesLeft = size();
 
-
-            @Override
             public boolean hasNext() {
-                return index<HASH_MAP_SIZE;
+                return index < HASH_MAP_SIZE && numEntriesLeft > 0;
             }
 
-            @Override
             public K next() {
                 while(map[index] == null) {
                     index++;
                 }
+                numEntriesLeft--;
                 return map[index++].getKey();
             }
         };
@@ -131,6 +137,29 @@ public class GMap<K, V> {
         return false;
 	}
 
+    private int findKey(K key) {
+        int entryIndex = 0;
+        for(GEntry<K, V> currEntry: map) {
+            if(currEntry != null) {
+                if(currEntry.getKey() == key) {
+                    break;
+                }
+            }
+            entryIndex++;
+        }
+        return entryIndex;
+    }
+
+	private void reHash() {
+        HASH_MAP_SIZE = 2*HASH_MAP_SIZE;
+		GEntry<K, V>[] newMap = new GEntry[HASH_MAP_SIZE];
+
+        for(int index=0; index < map.length; index++) {
+            newMap[index] = map[index];
+        }
+        map = newMap;
+	}
+
     public String toString() {
         return Arrays.toString(map);
     }
@@ -160,7 +189,6 @@ public class GMap<K, V> {
 			this.value = value;
 		}
 
-        @Override
         public String toString() {
             return "GEntry{" +
                     "key=" + key +
--- HashMap/Test.java
+++ HashMap/Test.java
@@ -8,15 +8,23 @@ public class Test {
 		myMap.put("a", 1);
 		myMap.put("b", 2);
 		myMap.put("c", 3);
-		//myMap.put("d", 4);
+		myMap.put("d", 4);
 		myMap.put("e", 5);
+		myMap.put("f", 6);
 		System.out.println(myMap.toString());
 
 		System.out.println("\n\nHashMap after duplicate key added:");
 		myMap.put("a", 25);
 		System.out.println(myMap.toString());
 
-		System.out.println("\nIterator object moving through HashMap entries: ");
+        System.out.println("\nValue at key 'a' ");
+        System.out.println(myMap.get("a"));
+
+        System.out.println("\nRemove 'a' ");
+        System.out.println(myMap.remove("a"));
+
+
+        System.out.println("\nIterator object moving through HashMap entries: ");
 		Iterator itr = myMap.entries();
 		while(itr.hasNext()) {
 			System.out.println(itr.next());
@@ -27,6 +35,7 @@ public class Test {
 		while(itr2.hasNext()) {
 			System.out.println(itr2.next());
 		}
+
 
 	}
 }
