f0f3bbe051f8a340b301e08b10f63c8394629905
Gary Mac Elhinney <Gary.mac-elhinney@ucdconnect.ie>
2016-08-01 11:32:17 +0100

    Refacting project initial upload

--- GPSMaster/src/GlobalPosition.java
+++ GPSMaster/src/GlobalPosition.java
@@ -2,29 +2,39 @@
  * Created by Gary on 25-Nov-15.
  */
 public class GlobalPosition {
-    private double latitude, longitude, elevation;
-    private Time time;
+        private double latitude, longitude, elevation;
+        private Time time;
 
-    public GlobalPosition(double latitude, double longitude, double elevation, String time) {
-        this.latitude = latitude;
-        this.longitude = longitude;
-        this.elevation = elevation;
-        this.time = new Time(time);
-    }
-    public double getLatitude() {
-        return latitude;
-    }
+        public GlobalPosition(double latitude, double longitude, double elevation, String time) {
+                this.latitude = latitude;
+                this.longitude = longitude;
+                this.elevation = elevation;
+                this.time = new Time(time);
+        }
 
-    public double getLongitude() {
-        return longitude;
-    }
+        public double getLatitude() {
+                return latitude;
+        }
 
-    public double getElevation() {
-        return elevation;
-    }
+        public double getLongitude() {
+                return longitude;
+        }
 
-    public Time getTime() {
-        return time;
-    }
+        public double getElevation() {
+                return elevation;
+        }
 
+        public Time getTime() {
+                return time;
+        }
+
+        @Override
+        public String toString() {
+                return "GlobalPosition{" +
+                        "latitude=" + latitude +
+                        ", longitude=" + longitude +
+                        ", elevation=" + elevation +
+                        ", time=" + time +
+                        '}';
+        }
 }
